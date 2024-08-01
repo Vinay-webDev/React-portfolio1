47acedb24afded2c68fdd2561366d7b5d6719cc2
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-08-01 23:58:34 +0530

    styling

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -7,35 +7,35 @@ import arrow from '../../assets/arrowright3.png';
 const favoriteTopics = [
                         { id:'1',
                           name:'Web Design',
-                          des:'The process of creating the visual layout, user interface, and user experience of a website.'
+                          des:'The process of creating the visual layout...'
                         },
                         { id:'2',
                           name:'Web Development',
-                          des:'The process of building and maintaining websites, involving both front-end (client-side) and back-end (server-side) programming.'
+                          des:'The process of building and maintaining websites...'
                         },
                         { id:'3',
                           name:'Data Structures and Algorithms',
-                          des:'Organized ways to store and manipulate data, and step-by-step procedures for solving computational problems.'
+                          des:'Organized ways to store and manipulate data...'
                         },
                         { id:'4',
                           name:'Front-end',
-                          des:'The client-side part of web development, dealing with the visual aspects and user interactions of a website.'
+                          des:'The client-side part of web development...'
                         },
                         { id:'5',
                           name:'React.js',
-                          des:'A JavaScript library for building user interfaces, particularly single-page applications, using a component-based architecture.'
+                          des:'A JavaScript library for building user interfaces...'
                         },
                         { id:'6',
                           name:'Back-end',
-                          des:' The server-side part of web development, focusing on databases, server logic, and application integration.'
+                          des:' The server-side part of web development...'
                         },
                         { id:'7',
                           name:'Node.js',
-                          des:'A runtime environment that allows you to run JavaScript on the server side, enabling server-side scripting and building scalable network applications.'
+                          des:'A runtime environment that allows you to run...'
                         },
                         { id:'8',
                           name:'TypeScript',
-                          des:'A typed superset of JavaScript that adds static types, enhancing code quality and maintainability.'
+                          des:'A typed superset of JavaScript that adds static...'
                         }
 ];
 
