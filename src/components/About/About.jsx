9aa068045d843db8072b5063a752a37d8859c533
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-08-01 18:48:15 +0530

    About component structure

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './About.css';
+import theme from '../../assets/theme2.png';
+import pfp from '../../assets/pfp.jpg';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <div className="about">
+      <div className="about-title">
+        <h1>About</h1>
+        <img src={theme} alt="theme"/>
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={pfp} alt="pfp"/>
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>I'm Chad from India bangalore, I've been working for the past 10 years as a full-stack web developer</p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"70%"}} /></div>
+            <div className="about-skill"><p>Javascript</p><hr style={{width:"80%"}} /></div>
+            <div className="about-skill"><p>React js</p><hr style={{width:"70%"}} /></div>
+            <div className="about-skill"><p>Nest js</p><hr style={{width:"50%"}} /></div>
+            <div className="about-skill"><p>Node js</p><hr style={{width:"60%"}} /></div>
+            <div className="about-skill"><p>Express js</p><hr style={{width:"50%"}} /></div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achivements">
+        <div className="about-achivement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achivement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED!</p>
+        </div>
+        <div className="about-achivement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About
