9d9a7cfe3e5b403e42057b7bf52d9de3c0730f41
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-08-02 22:58:32 +0530

    icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Contact.css';
+import theme from '../../assets/theme2.png';
+import phone from '../../assets/phone2.png';
+import location from '../../assets/location.png';
+import mail from '../../assets/mail2.png';
+
+
+
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+        <div className="contact-title">
+            <h1>Contact me</h1>
+            <img src={theme} alt="theme"/>
+        </div>
+    </div>
+  );
 }
 
 export default Contact
