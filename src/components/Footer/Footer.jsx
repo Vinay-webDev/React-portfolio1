96e9c9efae10de48cd88b7a3b7f7c6daaad56c1d
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-08-08 01:16:42 +0530

    fix for text decoration

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css';
-import logo from '../../assets/chad.png';
+import logo from '../../assets/new/chad.png';
 import user from '../../assets/new/personwhite.png';
 
 
