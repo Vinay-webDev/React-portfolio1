a971c57da5ae4ad14f47cc03e0d264a13c59a8df
Vinay-webDev <justchatting3000@gmail.com>
2024-12-17 18:11:28 +0530

    about section complete new modification:)

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,51 +12,39 @@ const About = () => {
         <img src={theme} alt="theme"/>
       </div>
       <div className="about-sections">
-        <div className="about-left">
-          <img src={pfp} alt="pfp"/>
-        </div>
         <div className="about-right">
           <div className="about-para">
             <p>I'm Vinay h c passionate web developer and I'm an undergraduate, my expertise lies in MERN stack. I enjoy building full-stack applications that deliver seamless user experiences.</p>
           </div>
-          <div className="about-skills">
-            <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"40%"}} /></div>
-            <div className="about-skill"><p>Javascript</p><hr style={{width:"60%"}} /></div>
-            <div className="about-skill"><p>React js</p><hr style={{width:"45%"}} /></div>
-            <div className="about-skill"><p>Node js</p><hr style={{width:"50%"}} /></div>
-            <div className="about-skill"><p>Express js</p><hr style={{width:"50%"}} /></div>
-          </div>
         </div>
         
       </div>
-          <h1 id="h1skills" >Skills</h1>
           <div className="skills-wrapper">
             <div className="skills-section">
               <p>Languages and Frameworks</p>
               <div className="skills-grid">
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
-                <img src="/public/logos/js.png" alt="js" />
+                <img src="/logos/html-5.png" alt="html-5" />
+                <img src="/logos/css-3.png" alt="css-3" />
+                <img src="/logos/js.png" alt="js" />
+                <img src="/logos/python.png" alt="python" />
+                <img src="/logos/reactjs.png" alt="reactjs" />
+                <img src="/logos/nodejs.png" alt="nodejs" />
+                <img src="/logos/expressjswhite.png" alt="expressjs" />
+                <img src="/logos/tailwindcss.png" alt="tailwindcss" />
+                <img src="https://user-images.githubusercontent.com/958486/218346783-72be5ae3-b953-4dd7-b239-788a882fdad6.svg" alt="zustand" />
+                <img src="/logos/jwt.png" alt="tailwindcss" />
               </div>
             </div>
 
             <div className="skills-section">
               <p>Database and Tools</p>
               <div className="skills-grid">
-                <img src="/public/logos/mongodb.png" alt="db" />
-                <img src="/public/logos/mongodb.png" alt="db" />
-                <img src="/public/logos/mongodb.png" alt="db" />
-                <img src="/public/logos/mongodb.png" alt="db" />
-                <img src="/public/logos/mongodb.png" alt="db" />
-                <img src="/public/logos/mongodb.png" alt="db" />
-                <img src="/public/logos/mongodb.png" alt="db" />
-                <img src="/public/logos/mongodb.png" alt="db" />
+                <img src="/logos/mongodbnew.png" alt="db" />
+                <img src="/logos/redis.png" alt="db" />
+                <img src="/logos/firebase.png" alt="db" />
+                <img src="/logos/git.png" alt="db" />
+                <img src="/logos/github-logo.png" alt="db" />
+                <img src="/logos/mongodbnew.png" alt="db" />
               </div>
             </div>
           </div>
@@ -65,6 +53,23 @@ const About = () => {
 }
 
 export default About
+
+/*
+<div className="about-skills">
+            <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"40%"}} /></div>
+            <div className="about-skill"><p>Javascript</p><hr style={{width:"60%"}} /></div>
+            <div className="about-skill"><p>React js</p><hr style={{width:"45%"}} /></div>
+            <div className="about-skill"><p>Node js</p><hr style={{width:"50%"}} /></div>
+            <div className="about-skill"><p>Express js</p><hr style={{width:"50%"}} /></div>
+          </div>
+*/
+
+
+/*
+<div className="about-left">
+          <img src={pfp} alt="pfp"/>
+        </div>
+*/
 
 /*
 <div className="about-achivement">
